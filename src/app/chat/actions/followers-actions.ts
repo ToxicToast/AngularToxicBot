be7f3b3f66d40250182c3de6e156504cb61c4f24
Kortyka Thomas <tkortyka@alegri.eu>
2018-02-27 17:28:05 +0100

    followers actions

---
+++ src/app/chat/actions/followers-actions.ts
@@ -0,0 +1,6 @@
+
+
+export enum FollowersActionTypes {
+  CALL_FOLLOWERSONLY = '[CHAT] Enable followers-only on a channel.',
+  CALL_FOLLOWERSONLYOFF = '[CHAT] Disabled followers-only on a channel.'
+}
