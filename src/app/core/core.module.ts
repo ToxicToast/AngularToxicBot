f88d77b0b0c54cc6b4ddad7d8b6ccbad954edf44
Kortyka Thomas <tkortyka@alegri.eu>
2018-02-28 16:44:08 +0100

    added error, loading, modal and splashscreen components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '@core/components/loading/loading.component';
+import { ErrorComponent } from '@core/components/error/error.component';
+import { SplashScreenComponent } from '@core/Components/splash-screen/splash-screen.component';
+import { ModalComponent } from '@core/Components/modal/modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    LoadingComponent,
+    ErrorComponent,
+    SplashScreenComponent,
+    ModalComponent
+  ]
 })
 export class CoreModule { }
